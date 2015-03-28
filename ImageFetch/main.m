41561c3d285d27df60e0b8e31e00d453743a7d69
jimtoepel <jim.toepel@gmail.com>
2015-03-28 15:45:04 -0700

    Final size check

--- ImageFetch/main.m
+++ ImageFetch/main.m
@@ -26,7 +26,7 @@ int main(int argc, const char * argv[]) {
         NSLog(@"The file is %lu bytes", (unsigned long)[data length]);
         
         BOOL written = [data writeToFile:@"/tmp/google.png"
-                              options:0
+                              options:NSDataWritingAtomic
                                 error:&error];
         
         if (!written) {
@@ -35,6 +35,9 @@ int main(int argc, const char * argv[]) {
         }
         
         NSLog(@"Success!");
+        
+        NSData *readData = [NSData dataWithContentsOfFile:@"/tmp/google.png"];
+        NSLog(@"The file read from the dis has %lu bytes", (unsigned long) [readData length]);
         
     }
     return 0;
