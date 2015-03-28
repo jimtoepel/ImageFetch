b70c74313530a7b2b603ea5fad42990fccda38d4
jimtoepel <jim.toepel@gmail.com>
2015-03-28 15:13:03 -0700

    initial commit

---
+++ ImageFetch/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ImageFetch
+//
+//  Created by Jim Toepel on 3/28/15.
+//  Copyright (c) 2015 FunderDevelopment. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
