3939b2e8e46ea0ebe09348cb750cc34558109cb9
jimtoepel <jim.toepel@gmail.com>
2015-03-28 15:26:15 -0700

    Downloads images

--- ImageFetch/main.m
+++ ImageFetch/main.m
@@ -10,8 +10,32 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+
+        NSURL *url = [NSURL URLWithString:@"http://www.google.com/images/logos/ps_logo2.png" ];
+        NSURLRequest *request = [NSURLRequest requestWithURL: url];
+        NSError *error = nil;
+        NSData *data = [NSURLConnection sendSynchronousRequest:request
+                                             returningResponse:NULL
+                                                         error:&error];
+        
+        if (!data) {
+            NSLog(@"fetch failed: %@", [error localizedDescription]);
+            return 1;
+        }
+        
+        NSLog(@"The file is %lu bytes", (unsigned long)[data length]);
+        
+        BOOL written = [data writeToFile:@"/tmp/google.png"
+                              options:0
+                                error:&error];
+        
+        if (!written) {
+            NSLog(@"write failed: %@", [error localizedDescription]);
+            return 1;
+        }
+        
+        NSLog(@"Success!");
+        
     }
     return 0;
 }
